8d30a08e3d57d1851181e44570d1f1877de81db9
Renan Lage <renalage@gmail.com>
2016-05-23 23:29:26 -0300

    Implement CRUD api for tasks in Redis

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
-var redis = require("redis"),
+var uuid = require('node-uuid');
+var redis = require('redis'),
     client = redis.createClient();
 
+
+var Task = function(totalSteps, extra) {
+    var nowUtc = new Date().getTime();
+    this.currentStep = 0;
+    this.totalSteps = totalSteps;
+    this.dateCreated = nowUtc;
+    this.dateUpdated = nowUtc;
+
+    if (extra !== undefined) {
+        this.extra = extra;
+    }
+};
+
 // Redis key -> value format:
-//   task_id:user_id:date -> curent_step
+// taskId -> {currentStep: int, totalSteps: int, extra: object,
+//            dateCreated: utc, dateUpdated: utc}
+
 
 // Initialize a new task in redis
-router.get('/tasks', function(req, res) {
-    task_id = req.body.task_id;
-    user_id = req.body.user_id;
-    total_steps = req.body.total_steps;
+router.post('/tasks', function(req, res) {
+    var json = req.body;
+    var task = new Task(json.totalSteps, json.extra);
+    var taskId = uuid.v1();
 
-    res.json({});
+    client.hmset(taskId, task, function(err, reply) {
+        res.json({taskId: taskId});
+    });
 });
 
+// Get tasks progress and details by id
+router.get('/tasks/:id', function(req, res) {
+    var task_id = req.params.id;
 
-// Get task progress and details
-router.get('/tasks/:task_id', function(req, res) {
-    res.json(req.params);
+    client.hgetall(task_id, function(err, obj) {
+        res.json({task: obj});
+    });
 });
 
 
 // Increment task progress by one step
-router.put('/tasks/:task_id', function(req, res) {
-    res.json({});
+router.put('/tasks/:id', function(req, res) {
+    var task_id = req.params.id;
+    var nowUtc = new Date().getTime();
+
+    client.hincrby(task_id, 'currentStep', 1, function(err, reply) {
+        res.json({currentStep: reply});
+    });
 });
 
 
-// End task
-router.delete('/tasks/:task_id', function(req, res) {
-    res.json({});
+// Delete task
+router.delete('/tasks/:id', function(req, res) {
+    var task_id = req.params.id;
+
+    client.del(task_id, function(err, reply) {
+        res.sendStatus(200);
+    });
 });
 
 
